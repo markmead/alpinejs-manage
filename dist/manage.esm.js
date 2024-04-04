d2fdd80bf60de3c529b3f4575f98748a1bb8df01
Mark Mead <itsmarkmead@gmail.com>
2024-04-04 11:55:41 +0100

    Hotfix - Bump packages

--- dist/manage.esm.js
+++ dist/manage.esm.js
@@ -1,1 +1,1 @@
-function r(t){t.magic("manage",()=>a=>{let e=document.querySelector(a);if(!!e)return t.$data(e)})}var u=r;export{u as default};
+function r(t){t.magic("manage",()=>a=>{let e=document.querySelector(a);if(e)return t.$data(e)})}var u=r;export{u as default};
